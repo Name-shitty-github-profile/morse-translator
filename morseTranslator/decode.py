589520f56ba8b72dd9275e6f56c02c4cd548dbb5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-08-13 22:37:21 +0200

    Update decode.py

--- morseTranslator/decode.py
+++ morseTranslator/decode.py
@@ -2,5 +2,6 @@ from .data import Letter
 def decode(content) -> str:
   deco: str = ''
   for thing in content.split(' '): 
-    if thing != '': deco += f'{Letter[thing]}'
+    if thing != '': 
+      deco += Letter[thing]
   return deco.lower()
