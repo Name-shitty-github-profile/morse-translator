444a680a82941e0687b03a6378307819a89d265e
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-12-18 10:33:21 -0500

    Update encode.py

--- morseTranslator/encode.py
+++ morseTranslator/encode.py
@@ -2,5 +2,5 @@ from .data import Morse
 def encode(content) -> str:
   content = content.replace(" ", "").replace(".", "").replace("_", ".")
   for letter in Morse.keys():
-     content.replace(letter, f"{Morse[letter]} ")
+     content = content.replace(letter, f"{Morse[letter]} ")
   return content[:-1]
